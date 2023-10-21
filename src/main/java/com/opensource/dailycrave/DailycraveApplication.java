3116f0f60cbe68b26927853ad9227489bffc9d39
Khalid Alnowaiser <khalid.alnowaiser@edfapay.com>
2023-10-21 11:12:09 +0300

    initialize project

---
+++ src/main/java/com/opensource/dailycrave/DailycraveApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.dailycrave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailycraveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailycraveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opensource/dailycrave/DailycraveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensource.dailycrave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailycraveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
